981bc836b115e83a164b47d54f4315b521e1c9b4
t2023-m0081 <t2023-m0081@t2023-m0081ui-MacBookAir.local>
2024-07-18 20:18:37 +0900

    :sparkles: Feat : 시장 api 호출

--- src/app/(provider)/(root)/local-food/page.tsx
+++ src/app/(provider)/(root)/local-food/page.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+'use client';
+import React, { useEffect } from 'react';
 
-const page = () => {
-  return <div>page</div>;
+const LocalFood = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/api/market');
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.error('데이터를 가져오지 못했습니다.', error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return <>시장페이지</>;
 };
 
-export default page;
+export default LocalFood;
