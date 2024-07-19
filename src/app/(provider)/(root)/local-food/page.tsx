1c9d52c195cfeb4569d39fa8a679ce1b7b17daee
t2023-m0081 <t2023-m0081@t2023-m0081ui-MacBookAir.local>
2024-07-19 13:59:02 +0900

    commit for pr

--- src/app/(provider)/(root)/local-food/page.tsx
+++ src/app/(provider)/(root)/local-food/page.tsx
@@ -1,21 +1,7 @@
-'use client';
-import React, { useEffect } from 'react';
+import React from 'react';
 
-const LocalFood = () => {
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('/api/market');
-        const data = await response.json();
-        console.log(data);
-      } catch (error) {
-        console.error('데이터를 가져오지 못했습니다.', error);
-      }
-    };
-    fetchData();
-  }, []);
-
-  return <>시장페이지</>;
+const page = () => {
+  return <div>page</div>;
 };
 
-export default LocalFood;
+export default page;
--- src/app/(provider)/(root)/market/page.tsx
+++ src/app/(provider)/(root)/market/page.tsx
@@ -1,7 +1,32 @@
-import React from 'react';
+'use client';
+import { Market } from '@/types/Market';
+import React, { useEffect, useState } from 'react';
 
-const page = () => {
-  return <div>page</div>;
+const MarketPage = () => {
+  const [markets, setMarkets] = useState<Market[]>([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/api/market');
+        const { data } = await response.json();
+        setMarkets(data);
+      } catch (error) {
+        console.error('데이터를 가져오지 못했습니다.', error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  console.log(markets);
+
+  return (
+    <>
+      {markets.map((market) => {
+        return <p key={market.시장코드}>{market.시장명}</p>;
+      })}
+      {}
+    </>
+  );
 };
 
-export default page;
+export default MarketPage;
