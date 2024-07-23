10efb35169230401e7ed8ab2e544f9ec2fe1b602
Sera Choi <c.sera2511@gmail.com>
2024-07-23 21:39:27 +0900

    :sparkles: feat : update conditional layout on a dynamic router

--- src/app/(provider)/(root)/local-food/layout.tsx
+++ src/app/(provider)/(root)/local-food/layout.tsx
@@ -1,9 +1,22 @@
+'use client';
+
 import DefaultAppLayout from '@/components/common/DefaultAppLayout';
+import { usePathname } from 'next/navigation';
 import { PropsWithChildren } from 'react';
 
 function LocalFoodLayout({ children }: PropsWithChildren) {
+  // TODO 조건부 TODO use client 쓰는 방법밖에 없는지 한 번 더 생각해보기
+  const pathLocalName: string = usePathname();
+  const showLocalFoodNavigation =
+    pathLocalName === '/local-food' ||
+    !pathLocalName.startsWith('/local-food/');
+
   return (
-    <DefaultAppLayout showHeader={true} showNavigation={true} headerTitle="특산물">
+    <DefaultAppLayout
+      showHeader={true}
+      showNavigation={showLocalFoodNavigation}
+      headerTitle="특산물"
+    >
       {children}
     </DefaultAppLayout>
   );
