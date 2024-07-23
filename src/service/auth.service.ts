06fe8366b6ca0f9ac724d9debcee1ca67f897a5c
chasyuss <cotn963@gmail.com>
2024-07-23 13:39:44 +0900

    :sparkles: Feat : 회원가입 미들웨어 + route 설정

---
+++ src/service/auth.service.ts
@@ -0,0 +1,28 @@
+import { Tables } from '@/types/supabase';
+import { AxiosInstance } from 'axios';
+
+type UserType = Tables<'users'>
+
+class AuthAPI {
+  private axios: AxiosInstance;
+
+  constructor(axios: AxiosInstance) {
+    this.axios = axios;
+  }
+
+//   async logIn(email: string, password: string) {
+//     const path = '/api/auth/log-in';
+
+//     const response = await this.axios.post<UserType>(path, { email, password });
+
+//     return response.data;
+//   }
+
+  async signUp(email: string, password: string, nickname?: string) {
+    const path = '/api/auth/sign-up';
+    const response = await this.axios.post<UserType>(path, { email, password, nickname });
+    return response.data;
+  }
+}
+
+export default AuthAPI;
