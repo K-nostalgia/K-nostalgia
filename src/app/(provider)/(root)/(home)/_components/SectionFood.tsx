cb22319623f7fb42b2fa010bdccb27f045ea2a3b
t2023-m0081 <t2023-m0081@t2023-m0081ui-MacBookAir.local>
2024-07-26 00:42:00 +0900

    :lipstick: style : 메인페이지

---
+++ src/app/(provider)/(root)/(home)/_components/SectionFood.tsx
@@ -0,0 +1,29 @@
+import { Tables } from '@/types/supabase';
+import { FoodBox } from './FoodBox';
+import Link from 'next/link';
+
+export const SectionFood = async () => {
+  const response = await fetch(`http://localhost:3000/api/localfood`);
+  const data: Tables<'local_food'>[] = await response.json();
+
+  return (
+    <div className="bg-secondary-normal">
+      <div className="flex flex-col justify-center items-center">
+        <h2 className="text-2xl text-label-light my-10 mx-">지역 특산물</h2>
+        <ul className="grid xs:grid-cols-2 gap-6 mx-[10px]">
+          {data.slice(0, 4).map((item) => {
+            return <FoodBox key={item.product_id} item={item}></FoodBox>;
+          })}
+        </ul>
+        <Link
+          href="/local-food"
+          className="border-label-disable border rounded-[12px] w-[95%] my-10"
+        >
+          <button className="w-full  transition-all duration-300 ease-in-out px-4 py-3 group-hover:hover:bg-label-disable text-label-light">
+            더 보기
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
---
+++ src/app/(provider)/(root)/(home)/_components/SectionMarket.tsx
@@ -0,0 +1,9 @@
+export const SectionMarket = () => {
+  return (
+    <div className="bg-label-disable">
+      <div className="flex flex-col justify-center items-center">
+        <h2 className="text-2xl text-primary-heavy my-10 mx-">유명 전통시장</h2>
+      </div>
+    </div>
+  );
+};
