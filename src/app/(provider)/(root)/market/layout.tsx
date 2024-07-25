683ba7b49e25789422725902af634e399c1efe69
Sera Choi <c.sera2511@gmail.com>
2024-07-26 00:20:06 +0900

    :sparkles: feat : update market layout

--- src/app/(provider)/(root)/market/layout.tsx
+++ src/app/(provider)/(root)/market/layout.tsx
@@ -1,9 +1,18 @@
 import DefaultAppLayout from '@/components/common/DefaultAppLayout';
+import { usePathname } from 'next/navigation';
 import { PropsWithChildren } from 'react';
 
 function MarketLayout({ children }: PropsWithChildren) {
+  const pathMarketName: string = usePathname();
+  const showMarketNavigation =
+    pathMarketName === '/market' || !pathMarketName.startsWith('/market/');
+
   return (
-    <DefaultAppLayout showHeader={true} showNavigation={true} headerTitle="전통시장">
+    <DefaultAppLayout
+      showHeader={true}
+      showNavigation={showMarketNavigation}
+      headerTitle="전통시장"
+    >
       {children}
     </DefaultAppLayout>
   );
