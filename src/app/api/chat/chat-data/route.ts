84d3dc1f411da31016573c2c9909489940c772db
Sera Choi <c.sera2511@gmail.com>
2024-07-27 17:02:02 +0900

    :recycle: refactor : refactor message and user

--- src/app/api/chat/chat-data/route.ts
+++ src/app/api/chat/chat-data/route.ts
@@ -4,12 +4,11 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (request:NextRequest) => {
     try{
-        const {data:chatData, error:chatError} = await supabase.from('chat').select('*').order('created_at', { ascending:true });
+        const {data:chatData, error:chatError} = await supabase.from('chat').select(`*, users(nickname, avatar)`).order('created_at', { ascending:true });
 
         if (chatError) {
             return NextResponse.json({error:chatError.message}, {status: 400});
         }
-        
     return NextResponse.json({data: chatData});
     } catch(error){
     return NextResponse.json({error: "Server error"}, {status: 500});
@@ -23,7 +22,7 @@ export const POST = async (request:NextRequest) => {
     const {data:chatData, error:chatError} = await supabase.from('chat').insert([{
 room_id, user_id, content
         }])
-        .select()
+        .select();
 
     if (chatError) {
         return NextResponse.json({error:chatError.message}, {status:400});
--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -22,9 +22,24 @@ import { Tables } from '@/types/supabase';
 import { useUser } from '@/hooks/useUser';
 import Image from 'next/image';
 import dayjs from 'dayjs';
+import { StringToBoolean } from 'class-variance-authority/types';
 
 // 채팅 아이디 가져와서 본인 아이디랑 같으면 오른쪽에 조건부 스타일링 + 다르면 왼족에 스타일링
 // xs일 때 가정 sm:max-w-[425px]
+
+interface chatUserType {
+  avatar: string;
+  nickname: string;
+}
+
+interface chatMessageType {
+  id: number;
+  created_at: string;
+  room_id: string;
+  user_id: string;
+  content: string;
+  users: chatUserType;
+}
 
 export function Chat() {
   const [message, setMessage] = useState<string>('');
@@ -39,9 +54,9 @@ export function Chat() {
   };
 
   const { data, isPending, error } = useQuery<
-    Tables<'chat'>[],
+    chatMessageType[],
     Error,
-    Tables<'chat'>[]
+    chatMessageType[]
   >({
     queryKey: ['chatData'],
     queryFn: fetchChatData
@@ -123,28 +138,33 @@ export function Chat() {
 
   // TODO supabase DB 하루마다 삭제하는 로직? 이 있을지 찾아보기
   // TODO 가끔 2개씩 전송되는 거 있는데 좀 더 확인해보기!
-  // TODO 로그인한 대상만 할 수 있게 처리
+  // TODO 로그인한 대상만 할 수 있게 처리_로그인 로직 끝나면 쿠키에서 가져오기 ? 로그인 안한 유저가 말하면 토스트 알림 보내기! ?
 
   // user 정보 가져오기 - 프로필 널일 때 처리
-  const featchUserData = async () => {
-    const response = await fetch('/api/chat/chat-user');
-    const { data: userData } = await response.json();
-    return userData;
-  };
+  // const featchUserData = async () => {
+  //   const response = await fetch('/api/chat/chat-user');
+  //   const { data: userData } = await response.json();
+  //   return userData;
+  // };
 
-  const { data: chatUserData } = useQuery<
-    Tables<'users'>[],
-    Error,
-    Tables<'users'>[]
-  >({
-    queryKey: ['chatUsers'],
-    queryFn: () => featchUserData()
-  });
+  // const { data: chatUserData } = useQuery<
+  //   Tables<'users'>[],
+  //   Error,
+  //   Tables<'users'>[]
+  // >({
+  //   queryKey: ['chatUsers'],
+  //   queryFn: () => featchUserData()
+  // });
 
   // 날짜 포맷
   const formatDate = (date: string) => {
     return dayjs(date).locale('ko').format('YYYY.MM.DD HH:MM');
   };
+
+  //TODO 스크롤 하단으로 유지
+  useEffect(() => {
+    window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+  }, [queryClient]);
 
   return (
     <Dialog>
@@ -155,7 +175,7 @@ export function Chat() {
       </DialogTrigger>
       {/*TODO 최소 크기일 때 max-w-[330px] 반응형일 때 조절하기  */}
       <DialogContent className="max-w-[330px] bg-normal rounded-xl">
-        <div className="border-b-2 w-[calc(100%+48px)] -mx-6 shadow-[rgba(0,0,0,0.14)_0px_2px_4px_0px]">
+        <div className="border-b-2 w-[calc(100%+33px)] -mx-4 shadow-[rgba(0,0,0,0.14)_0px_2px_4px_0px]">
           <DialogHeader>
             <DialogTitle className="mb-2 px-2 py-3">향그리움</DialogTitle>
             <DialogDescription>
@@ -165,16 +185,13 @@ export function Chat() {
         </div>
         <div className="grid gap-4 py-4 h-[400px] xs:h-[400px] flex-1 overflow-y-auto scrollbar-hide">
           {data?.map((item) => {
-            const TheUserProfile = chatUserData?.find(
-              (user) => user.id === item.user_id
-            );
             return item.user_id === user?.id ? (
               // 나일 경우
               <div key={item.id} className="flex flex-col gap-2">
-                {TheUserProfile?.avatar ? (
+                {item.users?.avatar ? (
                   <Image
-                    src={TheUserProfile?.avatar}
-                    alt={`${TheUserProfile?.nickname}의 프로필`}
+                    src={item.users?.avatar}
+                    alt={`${item.users?.nickname}의 프로필`}
                     height={36}
                     width={36}
                     className="rounded-full ml-auto w-9 h-9"
@@ -184,7 +201,7 @@ export function Chat() {
                     X
                   </div>
                 )}
-                <div className="border-2 border-primary-strong rounded-xl rounded-tr-none ml-auto text-white bg-primary-strong w-fit px-3 py-2">
+                <div className="border border-primary-strong rounded-xl rounded-tr-none ml-auto text-white bg-primary-strong w-fit px-3 py-2">
                   {item.content}
                 </div>
                 <div className="text-xs text-label-assistive ml-auto">
@@ -196,10 +213,10 @@ export function Chat() {
               <div key={item.id} className="flex flex-col gap-3 w-full">
                 <div className="flex gap-2">
                   {/* TODO null 일 경우 기본 이미지 태그로 바꾸기 */}
-                  {TheUserProfile?.avatar ? (
+                  {item.users?.avatar ? (
                     <Image
-                      src={TheUserProfile.avatar}
-                      alt={`${TheUserProfile.nickname}의 프로필`}
+                      src={item.users.avatar}
+                      alt={`${item.users.nickname}의 프로필`}
                       height={36}
                       width={36}
                       className="rounded-full w-9 h-9"
@@ -210,10 +227,10 @@ export function Chat() {
                     </div>
                   )}
                   <div className="flex items-center font-semibold mr-auto">
-                    {TheUserProfile?.nickname}
+                    {item.users?.nickname}
                   </div>
                 </div>
-                <div className="border-2 border-primary-strong rounded-xl rounded-tl-none w-fit px-3 py-2">
+                <div className="border border-primary-strong rounded-xl rounded-tl-none w-fit px-3 py-2">
                   {item.content}
                 </div>
                 <div className="text-xs text-label-assistive">
@@ -224,13 +241,13 @@ export function Chat() {
           })}
         </div>
 
-        <div className="border-t-2 w-[calc(100%+48px)] -mx-6 shadow-[rgba(31,30,30,0.08)_0px_-2px_8px_0px]">
+        <div className="border-t-2 w-[calc(100%+33px)] -mx-4 shadow-[rgba(31,30,30,0.08)_0px_-2px_8px_0px]">
           <DialogFooter className="xs:flex relative items-center">
             <div className="relative w-[87%] pt-4">
               <Input
                 type="text"
                 placeholder="메시지 보내기..."
-                className="pr-12 rounded-xl border-2 border-primary-strong placeholder:text-label-assistive"
+                className="pr-12 rounded-xl border border-primary-strong placeholder:text-label-assistive"
                 value={message}
                 onChange={handleMessage}
                 onKeyDown={handleKeyDownEnter}
