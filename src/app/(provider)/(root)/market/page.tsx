5a071a06149e7e31421bd7a7ff4a86ea7deba7aa
GA HYEON <dlrkxkszz@gmail.com>
2024-07-28 06:04:41 +0900

    :sparkles: feat : market pagination 기능 추가 및 테스트 중

--- src/app/(provider)/(root)/market/page.tsx
+++ src/app/(provider)/(root)/market/page.tsx
@@ -30,8 +30,10 @@ const MarketPage = () => {
   const [loading, setLoading] = useState(false);
   const [heart, setHeart] = useState<string[]>([]);
   const [filteredMarkets, setFilteredMarkets] =
-    useState<TotalMarketImagesType | null>(images);
+    useState<TotalMarketImagesType | null>(null);
   const [activeFilter, setActiveFilter] = useState<string>('전체');
+
+  // 시장 데이터 불러오는 API
   const fetchMarkets = async (page: number) => {
     try {
       const response = await fetch(`/api/market/marketList?page=${page}`);
@@ -69,14 +71,14 @@ const MarketPage = () => {
   useEffect(() => {
     const fetchImages = async () => {
       setLoading(true);
-      const results = [];
+      const results: TotalMarketImagesType = [];
 
       for (const market of markets) {
         try {
           const response = await fetch(
             `/api/market/marketImage?query=${encodeURIComponent(
               market.시장명
-            )}&display=1`
+            )}&display=2`
           );
           const data = await response.json();
           results.push({
@@ -96,11 +98,12 @@ const MarketPage = () => {
           });
         }
 
-        // 각 요청 사이에 0.15초 지연 추가(요청 속도가 너무 빨라서 이미지를 찾아올 수 가 없다는 에러가 뜸)
+        // 각 요청 사이에 0.15초 지연 추가(요청 속도가 너무 빨라서 이미지를 찾아올 수 가 없다는 에러발생)
         await new Promise((resolve) => setTimeout(resolve, 150));
       }
 
-      setImages(results);
+      setImages(results); // 시장 데이터 초기값(전체 필터 버튼 눌렀을때 초기상태)
+      setFilteredMarkets(results);
       setLoading(false);
     };
 
@@ -119,20 +122,26 @@ const MarketPage = () => {
   const handleClickFilterButton = (region: string) => {
     setActiveFilter(region);
     if (region === '전체') setFilteredMarkets(images);
-    setFilteredMarkets(images.filter((image) => image.시도 === region)); //images안에 item(시장정보)이 들어가있음
+    else setFilteredMarkets(images.filter((image) => image.시도 === region)); //images안에 item(시장정보)이 들어가있음
   };
+
+  const totalPages = Math.ceil(1388 / 10);
 
   return (
     <>
-      {marketFilterButtons.map((region) => (
-        <FilterButton
-          key={region}
-          onClick={() => handleClickFilterButton(region)}
-          isActive={activeFilter === region}
-        >
-          {region}
-        </FilterButton>
-      ))}
+      <div className="flex w-full">
+        {marketFilterButtons.map((region) => (
+          <div key={region} className="w-full">
+            <FilterButton
+              key={region}
+              onClick={() => handleClickFilterButton(region)}
+              isActive={activeFilter === region}
+            >
+              {region}
+            </FilterButton>
+          </div>
+        ))}
+      </div>
       <div>
         {loading ? (
           <p>로딩중...</p>
@@ -145,6 +154,7 @@ const MarketPage = () => {
                 )}/${encodeURIComponent(item.도로명주소)}?page=${currentPage}`}
               >
                 <h3>{item.시장명}</h3>
+                <h2>{item.도로명주소}</h2>
                 <div className="relative w-[140px] h-[140px]">
                   {item.image ? (
                     <Image
@@ -171,6 +181,42 @@ const MarketPage = () => {
             </div>
           ))
         )}
+      </div>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          marginTop: '20px'
+        }}
+      >
+        <button
+          onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
+          disabled={currentPage === 1}
+        >
+          prev
+        </button>
+        <div>
+          {[...Array(totalPages)].map((_, index) => (
+            <button
+              key={index + 1}
+              onClick={() => setCurrentPage(index + 1)}
+              style={{
+                fontWeight: currentPage === index + 1 ? 'bold' : 'normal',
+                margin: '0 5px'
+              }}
+            >
+              {index + 1}
+            </button>
+          ))}
+        </div>
+        <button
+          onClick={() =>
+            setCurrentPage((prev) => Math.min(prev + 1, totalPages))
+          }
+          disabled={currentPage === totalPages}
+        >
+          next
+        </button>
       </div>
     </>
   );
