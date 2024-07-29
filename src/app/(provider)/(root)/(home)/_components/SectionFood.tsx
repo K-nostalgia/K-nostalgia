167a515194456be7757e83cbcda5a04686859291
Sera Choi <c.sera2511@gmail.com>
2024-07-29 23:32:05 +0900

    fix : bugfix

--- src/app/(provider)/(root)/(home)/_components/SectionFood.tsx
+++ src/app/(provider)/(root)/(home)/_components/SectionFood.tsx
@@ -1,10 +1,26 @@
+'use client';
+
 import { Tables } from '@/types/supabase';
 import Link from 'next/link';
 import { FoodBox } from './FoodBox';
+import { useEffect, useState } from 'react';
 
-export const SectionFood = async () => {
-  const response = await fetch(`/api/localfood`);
-  const data: Tables<'local_food'>[] = await response.json();
+export const SectionFood = () => {
+  const [localFood, setLocalFood] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('/api/localfood');
+      const result = await response.json();
+      setLocalFood(result.data);
+      console.log(result);
+      setLocalFood(result.localFoodData);
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(localFood);
 
   return (
     <div className="bg-secondary-normal">
@@ -13,7 +29,7 @@ export const SectionFood = async () => {
           지역 특산물
         </h2>
         <ul className="grid grid-cols-2 gap-6 mx-[10px]">
-          {data.slice(0, 4).map((item, index) => {
+          {localFood?.slice(0, 4).map((item, index) => {
             return (
               <FoodBox
                 key={item.product_id}
--- src/app/(provider)/(root)/(home)/_components/SectionMarket.tsx
+++ src/app/(provider)/(root)/(home)/_components/SectionMarket.tsx
@@ -1,8 +1,24 @@
-import { Carousel } from './Carousel';
+'use client';
 
-export const SectionMarket = async () => {
-  const response = await fetch('/api/market/marketDetailList');
-  const { data } = await response.json();
+import { Carousel } from './Carousel';
+import { useEffect, useState } from 'react';
+
+export const SectionMarket = () => {
+  const [mainMarket, setMainMarket] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchMarketData = async () => {
+      const response = await fetch('/api/market/marketDetailList');
+      const result = await response.json();
+      setMainMarket(result.data);
+      console.log(result);
+      setMainMarket(result.data);
+    };
+
+    fetchMarketData();
+  }, []);
+
+  console.log(mainMarket);
 
   return (
     <div className="bg-[#FFF8EF]">
@@ -10,7 +26,7 @@ export const SectionMarket = async () => {
         <h2 className="text-2xl text-primary-heavy my-10 mx-10 font-custom">
           유명 전통시장
         </h2>
-        <Carousel data={data} />
+        <Carousel data={mainMarket} />
       </div>
     </div>
   );
