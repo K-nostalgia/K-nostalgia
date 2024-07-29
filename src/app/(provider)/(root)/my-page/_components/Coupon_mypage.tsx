18b60dabde76bded4e2c7cf67e6e94e5e668cbda
chasyuss <cotn963@gmail.com>
2024-07-29 18:06:09 +0900

    :sparkles: feat : mypage에서 쿠폰페이지로 넘어가기

--- src/app/(provider)/(root)/my-page/_components/Coupon_mypage.tsx
+++ src/app/(provider)/(root)/my-page/_components/Coupon_mypage.tsx
@@ -1,14 +1,24 @@
+import { useRouter } from 'next/navigation';
 import React from 'react';
 import { BsChevronRight } from 'react-icons/bs';
 import { PiTicketLight } from 'react-icons/pi';
 
 const Coupon_mypage = () => {
+  const router = useRouter();
+
+  const handleCouponClick = () => {
+    router.push('/coupon-page');
+  };
+
   return (
     <div className="p-4">
       <div className="flex items-center py-[3px]">
         <PiTicketLight className="mr-[8px] text-label-normal text-[20px]" />
         <span className="text-[16px]"> 할인 쿠폰 </span>
-        <BsChevronRight className="ml-auto text-[#545454]" />
+        <BsChevronRight
+          className="ml-auto text-[#545454] cursor-pointer"
+          onClick={handleCouponClick}
+        />
       </div>
     </div>
   );
