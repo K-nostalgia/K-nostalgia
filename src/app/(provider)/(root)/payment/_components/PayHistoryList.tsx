679ceb0b3969f8be970d91afd417d636a6e35107
jognhoon <jonghoon7431@gmail.com>
2024-07-30 20:18:25 +0900

    :sparkles: feat : ui 작업 완

--- src/app/(provider)/(root)/payment/_components/PayHistoryList.tsx
+++ src/app/(provider)/(root)/payment/_components/PayHistoryList.tsx
@@ -7,6 +7,7 @@ import api from '@/service/service';
 import { Tables } from '@/types/supabase';
 import { useQuery } from '@tanstack/react-query';
 import dayjs from 'dayjs';
+import 'dayjs/locale/ko';
 import Image from 'next/image';
 
 const PayHistoryList = () => {
@@ -18,13 +19,13 @@ const PayHistoryList = () => {
   const userId = users?.id;
 
   const { payHistoryList } = useGetPaymentHistoryWithSupabase(userId);
-
+  //TODO 로그인 안 되어있을경우 로딩 이외에 처리 추가
   if (!payHistoryList) {
     return <Loading />;
   }
 
   const ordersList = payHistoryList.reduce((acc: any, order: any) => {
-    const date = dayjs(order.payment_date).format('YYYY-MM-DD');
+    const date = dayjs(order.payment_date).format('YYYY. MM. DD');
     if (!acc[date]) {
       acc[date] = [];
     }
@@ -37,49 +38,80 @@ const PayHistoryList = () => {
 
   console.log(ordersList);
   return (
-    <div>
+    <div className="p-[16px] border-t-4 border-[#F2F2F2] mb-[70px]">
       {sortedDates.map((date) => (
-        <div key={date} className="">
-          <div className="flex gap-[8px]">
+        <div key={date}>
+          <div className="flex gap-[8px] ml-[4px]">
             <p className="font-medium">{date}</p>
             <p className="font-medium">주문</p>
           </div>
 
           {ordersList[date].map((order: any) => (
-            <div key={order.id} className="border p-4 mb-2 rounded">
-              <p className="font-medium">
-                {order.status === 'CANCELLED' ? '주문취소완료' : '상품준비중'}
-              </p>
-              {order.products.map((product: any) => (
-                <div key={product.id} className="flex">
+            <div
+              key={order.id}
+              className="border rounded-[12px] p-[16px] mt-[8px] mb-[16px]"
+            >
+              <div className="flex gap-[4px]">
+                <p className="font-medium">
+                  {order.status === 'CANCELLED' ? '주문취소완료' : '상품준비중'}
+                </p>
+                <p className="text-[#AFAFAF]">·</p>
+                <div className="flex">
+                  {order.status === 'PAID' && (
+                    <p className="text-[#9C6D2E] font-normal">
+                      {dayjs(order.payment_date)
+                        .locale('ko')
+                        .format('MM/DD (ddd)')}{' '}
+                      도착
+                    </p>
+                  )}
+                </div>
+              </div>
+
+              {order.products.map((product: any, index: number) => (
+                <div
+                  key={product.id}
+                  className={`flex gap-[12px] pt-[12px] ${
+                    index !== order.products.length - 1
+                      ? 'border-b-2 pb-[12px]'
+                      : ''
+                  }`}
+                >
                   <Image
                     src={imageSrc(product.name)}
                     width={64}
                     height={64}
                     style={{
                       width: 64,
-                      height: 64
+                      height: 64,
+                      objectFit: 'cover',
+                      borderRadius: 8
                     }}
                     alt={product.name}
                   />
-                  <div className="flex flex-col">
-                    <p>{product.name}</p>
-                    <div className="flex">
-                      <p>{product.amount}원</p>
+
+                  <div className="flex flex-col justify-center gap-[8px]">
+                    <p className="font-medium">{product.name}</p>
+                    <div className="flex text-[#79746D] gap-[4px]">
+                      <p>{product.amount.toLocaleString()}원</p>
                       <p>·</p>
                       <p>{product.quantity}개</p>
                     </div>
                   </div>
+                  {index !== order.products.length - 1 && <hr />}
                 </div>
               ))}
               <div
-                //TODO 버튼 임의로 띄워두고 스타일 지정
-                className={`flex gap-[20px] ${
+                className={`flex gap-[7px] text-[14px] font-semibold pt-[12px] ${
                   order.status === 'CANCELLED' ? 'hidden' : 'flex'
                 }`}
               >
-                <button>주문취소</button>
-                <button>배송조회</button>
+                <button className="py-[10px] px-[16px] border-[1px] border-[#AFAFAF] text-[#79746D] w-[152px] h-[40px] rounded-[10px]">
+                  주문취소
+                </button>
+                <button className="py-[10px] px-[16px] border-[1px] border-[#9C6D2E] text-[#9C6D2E] w-[152px] h-[40px] rounded-[10px]">
+                  배송조회
+                </button>
               </div>
             </div>
           ))}
--- src/app/(provider)/(root)/payment/page.tsx
+++ src/app/(provider)/(root)/payment/page.tsx
@@ -4,7 +4,6 @@ const PaymentList = () => {
   //TODO 결제 다건 조회로 변경 사항 업데이트 하는 로직?
   return (
     <div>
-      주문내역페이지
       <PayHistory />
     </div>
   );
