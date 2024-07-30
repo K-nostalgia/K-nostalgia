6f9f717ba86d3838e17c1b47a1ff724d2ded3f42
t2023-m0081 <t2023-m0081@t2023-m0081ui-MacBookAir.local>
2024-07-30 23:58:44 +0900

    :lipstick: style : 메인 배너 슬라이드 추가

---
+++ src/app/(provider)/(root)/(home)/_components/SectionBanner.tsx
@@ -0,0 +1,35 @@
+'use client';
+import { Autoplay } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { banners } from '@/lib/banners';
+import Image from 'next/image';
+
+export const SectionBanner = () => {
+  return (
+    <Swiper
+      // install Swiper modules
+      modules={[Autoplay]}
+      spaceBetween={16}
+      slidesPerView={'auto'}
+      centeredSlides={true}
+      autoplay={{ delay: 2000, disableOnInteraction: false }}
+      loop={true}
+    >
+      {banners.map((img, index) => (
+        <SwiperSlide key={index}>
+          <Image
+            src={img}
+            width={375}
+            height={315}
+            alt={`메인 배너 이미지 ${index + 1}`}
+          />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
--- src/app/(provider)/(root)/(home)/page.tsx
+++ src/app/(provider)/(root)/(home)/page.tsx
@@ -1,9 +1,11 @@
+import { SectionBanner } from './_components/SectionBanner';
 import { SectionFood } from './_components/SectionFood';
 import { SectionMarket } from './_components/SectionMarket';
 
 const Homepage = () => {
   return (
     <>
+      <SectionBanner />
       <SectionFood />
       <SectionMarket />
     </>
