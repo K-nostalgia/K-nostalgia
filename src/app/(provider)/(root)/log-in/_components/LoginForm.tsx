c989e253108a8d309b1c1f04bb4e9c29b0569e94
chasyuss <cotn963@gmail.com>
2024-07-30 19:25:16 +0900

    :sparkles: feat : 아이콘 클릭시 비밀번호 text로 보여지기

--- src/app/(provider)/(root)/log-in/_components/LoginForm.tsx
+++ src/app/(provider)/(root)/log-in/_components/LoginForm.tsx
@@ -2,17 +2,13 @@
 import React, { useState } from 'react';
 import api from '@/service/service';
 import { useRouter } from 'next/navigation';
-import { useUser } from '@/hooks/useUser';
+import { PiEye } from 'react-icons/pi';
 
 const LoginForm = () => {
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
   const router = useRouter();
-  // const { data: user, isLoading, error } = useUser();
-
-  // console.log('user', user);
-  // if (!user) return;
-  // console.log('userid', user.id);
+  const [showPassword, setshowPassword] = useState(false);
 
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -25,6 +21,10 @@ const LoginForm = () => {
       console.error('로그인 실패', error);
       alert('로그인 실패 ');
     }
+  };
+
+  const handleShowPassword = () => {
+    setshowPassword(!showPassword);
   };
 
   const handleClickKaKaoLogin = async () => {
@@ -51,29 +51,31 @@ const LoginForm = () => {
   return (
     <form onSubmit={handleLogin} className="space-y-6">
       <div>
-        <label className="block text-sm font-bold text-label-normal mb-2">
-          이메일
-        </label>
+        <label className="block text-sm text-label-normal mb-2">이메일</label>
         <input
           type="email"
           placeholder="이메일을 입력해 주세요"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-          className="w-full px-3 py-2 border border-label-assistive rounded-xl focus:outline-none focus:border-primary-strong text-label-normal"
+          className="w-full px-3 py-2 border border-label-assistive rounded-xl focus:outline-none focus:border-primary-strong text-primary-20"
         />
       </div>
-      <div>
-        <label className="block text-sm font-bold text-label-normal mb-2">
-          비밀번호
-        </label>
+
+      <div className="relative">
+        <label className="block text-sm text-label-normal mb-2">비밀번호</label>
         <input
-          type="password"
+          type={showPassword ? 'text' : 'password'}
           placeholder="비밀번호를 입력해 주세요"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-          className="w-full px-3 py-2 border border-label-assistive rounded-xl focus:outline-none focus:border-primary-strong text-label-normal"
+          className="w-full px-3 py-2 border border-label-assistive rounded-xl focus:outline-none focus:border-primary-strong text-primary-20"
+        />
+        <PiEye
+          onClick={handleShowPassword}
+          className="absolute right-3 top-[52%] flex items-center cursor-pointer text-gray-500 text-[24px]"
         />
       </div>
+
       <button
         type="submit"
         className="w-full py-2 px-4 bg-label-disable text-white rounded-xl hover:bg-primary-strong"
