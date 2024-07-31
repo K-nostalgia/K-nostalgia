b048cd7bc642bce4b00124f09aefe8aeec420c5e
chasyuss <cotn963@gmail.com>
2024-07-31 17:45:06 +0900

    :sparkles: feat : sign-up 유효성 검사 추가

--- src/app/(provider)/(root)/sign-up/_components/SignUpContainer.tsx
+++ src/app/(provider)/(root)/sign-up/_components/SignUpContainer.tsx
@@ -6,6 +6,15 @@ import { GoArrowLeft } from 'react-icons/go';
 import steps from './Step';
 import Stepper from './Stepper';
 import SignupForm from './SignUpForm';
+import {
+  validateEmail,
+  validateName,
+  validatePassword
+} from '@/utils/validate';
+
+type ErrorState = {
+  [key: string]: string;
+};
 
 const SignUpContainer = () => {
   const [step, setStep] = useState(0);
@@ -17,16 +26,45 @@ const SignUpContainer = () => {
     name: ''
   });
   const router = useRouter();
+  const [errors, setErrors] = useState<ErrorState>({});
 
   //입력 필드의 변경 사항을 반영
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setUserInfo((prev) => ({ ...prev, [name]: value }));
+    setErrors((prev) => ({ ...prev, [name]: '' }));
+  };
+
+  const validateStep = () => {
+    const currentStep = steps[step];
+    const value = userInfo[currentStep.key as keyof typeof userInfo];
+    let error = '';
+
+    if (!value) {
+      error = '이 필드는 필수입니다.';
+    } else if (currentStep.key === 'email' && !validateEmail(value)) {
+      error = '형식에 맞지 않는 이메일 주소에요.';
+    } else if (currentStep.key === 'password' && !validatePassword(value)) {
+      error =
+        '영문 대문자, 소문자, 숫자와 !"?&@%$와 같은 특수문자를 포함하여 6글자 이상 입력하셔야해요.';
+    } else if (
+      currentStep.key === 'confirmPassword' &&
+      value !== userInfo.password
+    ) {
+      error = '비밀번호를 다시 확인해 주세요.';
+    } else if (currentStep.key === 'name' && !validateName(value)) {
+      error = '정확한 이름을 입력해주세요';
+    }
+
+    setErrors((prev) => ({ ...prev, [currentStep.key]: error }));
+    return !error;
   };
 
   const nextStep = () => {
-    if (step < steps.length - 1) {
-      setStep(step + 1);
+    if (validateStep()) {
+      if (step < steps.length - 1) {
+        setStep(step + 1);
+      }
     }
   };
 
@@ -37,10 +75,13 @@ const SignUpContainer = () => {
   };
 
   const handleSubmit = async () => {
-    if (userInfo.password !== userInfo.confirmPassword) {
-      alert('비밀번호가 일치하지 않습니다.');
+    if (!validateStep()) {
       return;
     }
+    // if (userInfo.password !== userInfo.confirmPassword) {
+    //   alert('비밀번호가 일치하지 않습니다.');
+    //   return;
+    // }
 
     console.log('userinfo 확인', userInfo);
 
@@ -88,6 +129,7 @@ const SignUpContainer = () => {
             name={steps[step].key}
             value={userInfo[steps[step].key as keyof typeof userInfo]}
             onChange={handleChange}
+            error={errors[steps[step].key]}
           />
         </div>
         <div className="flex justify-between">
--- src/app/(provider)/(root)/sign-up/_components/SignUpForm.tsx
+++ src/app/(provider)/(root)/sign-up/_components/SignUpForm.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
+import { PiEye, PiEyeSlash } from 'react-icons/pi';
 
 interface SignupFormProps {
   title: string;
@@ -7,6 +9,7 @@ interface SignupFormProps {
   name: string;
   value: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  error?: string;
 }
 
 const SignupForm: React.FC<SignupFormProps> = ({
@@ -15,24 +18,51 @@ const SignupForm: React.FC<SignupFormProps> = ({
   type,
   name,
   value,
-  onChange
+  onChange,
+  error
 }) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
-    <>
+    <div className="relative mb-6">
       <label className="block text-label-normal text-20px text-border mb-6 text-left">
         {title}
       </label>
       <label className="block text-label-normal text-sm font-bold mb-2">
         {label}
       </label>
-      <input
-        type={type}
-        name={name}
-        value={value}
-        onChange={onChange}
-        className="w-full px-3 py-2 border border-primary-strong rounded-xl focus:outline-none focus:border-primary-strong"
-      />
-    </>
+      <div className="relative">
+        <input
+          type={
+            type === 'password' ? (showPassword ? 'text' : 'password') : type
+          }
+          name={name}
+          value={value}
+          onChange={onChange}
+          placeholder={title}
+          className={`w-full px-3 py-2 border rounded-xl shadow-sm focus:outline-none ${
+            error ? 'border-red-500' : 'border-primary-strong'
+          }`}
+        />
+        {type === 'password' && (
+          <span
+            className="absolute inset-y-0 right-3 flex items-center cursor-pointer"
+            onClick={handleShowPassword}
+          >
+            {showPassword ? (
+              <PiEyeSlash className="text-[#545454] text-[24px]" />
+            ) : (
+              <PiEye className="text-[#545454] text-[24px]" />
+            )}
+          </span>
+        )}
+      </div>
+      {error && <p className="text-red-500 text-sm mt-1">{error}</p>}
+    </div>
   );
 };
 
