6fe5f6b02b55399c1b2aa38c200c7aa458e6f520
GA HYEON <dlrkxkszz@gmail.com>
2024-08-01 11:20:19 +0900

    :lipstick: style : market detail ui수정중

--- src/app/(provider)/(root)/market/[id]/page.tsx
+++ src/app/(provider)/(root)/market/[id]/page.tsx
@@ -3,6 +3,7 @@
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { MarketType } from '@/types/Market';
+import { PiMapPin } from 'react-icons/pi';
 export type ImagesType = {
   title: string;
   link: string;
@@ -34,25 +35,7 @@ const MarketDetailPage = ({ params }: { params: { id: string } }) => {
   useEffect(() => {
     const fetchImage = async () => {
       setLoading(true);
-
-      // if (market) {
-      //   try {
-      //     const response = await fetch(
-      //       `/api/market/marketImage?query=${encodeURIComponent(
-      //         market.시장명
-      //       )}&display=5`
-      //     );
-
-      //     const data = await response.json();
-      //     console.log('Image Data___', data);
-      //     setImages(data);
-      //   } catch (error) {
-      //     console.error('이미지를 가지고 오지 못했습니다.', error);
-      //     setImages(null);
-      //   }
-      // }
     };
-
     fetchImage();
   }, [market]);
 
@@ -61,40 +44,67 @@ const MarketDetailPage = ({ params }: { params: { id: string } }) => {
   const images = market.이미지 ?? [];
 
   return (
-    <div className="h-screen">
-      <h1>{market?.시장명}</h1>
-      {images.length > 0 && (
-        <div className="relative w-[140px] h-[140px]">
-          <Image
-            src={images[0]}
-            alt={market?.시장명 ?? 'market name'}
-            fill
-            sizes="(max-width: 768px) 100vw, 140px"
-            priority
-            className="absolute w-full h-full"
-            style={{ objectFit: 'cover' }}
-          />
-        </div>
-      )}
-      {images.slice(0).map((image, index) => {
-        if (index === 0) return null; // 0번째 인덱스는 스킵
-        return (
-          <div key={index}>
-            <div className="relative w-[343px] h-[220px]">
-              <Image
-                src={image}
-                alt={market?.시장명 ?? 'market name'}
-                fill
-                sizes="(max-width: 768px) 100vw, 343px"
-                priority
-                className="absolute w-full h-full"
-                style={{ objectFit: 'cover' }}
-              />
-            </div>
+    <>
+      <div className="w-full p-4 bg-normal">
+        {images.length > 0 && (
+          <div className="flex relative w-[343px] h-[230px] ">
+            <Image
+              src={images[0]}
+              alt={market?.시장명 ?? 'market name'}
+              fill
+              sizes="(max-width: 768px) 100vw, 140px"
+              priority
+              className="absolute w-full h-full rounded-[8px]"
+              style={{ objectFit: 'cover' }}
+            />
           </div>
-        );
-      })}
-    </div>
+        )}
+      </div>
+      <div className="flex flex-col justify-center items-center mt-4 mb-5">
+        <p className="text-xl font-semibold text-label-strong">
+          {market?.시장명}
+        </p>
+        <p className="text-sm font-normal text-label-alternative">
+          {market.도로명주소.split(' ').slice(0, 2).join(' ')}
+        </p>
+        <div className="w-[375px] h-2 bg-color-[#F2F2F2]" />
+      </div>
+      <div className="w-full p-4 bg-primary-20">
+        <div className="mt-4 mb-8 text-center text-xl font-semibold text-primary-90">
+          시장 전체 이미지
+        </div>
+        {images.slice(0).map((image, index) => {
+          if (index === 0) return null; // 0번째 인덱스는 스킵
+          return (
+            <div key={index}>
+              <div className="relative w-[343px] h-[280px] border border-primary-90 rounded-xl mb-4 ">
+                <Image
+                  src={image}
+                  alt={market?.시장명 ?? 'market name'}
+                  fill
+                  sizes="(max-width: 768px) 100vw, 343px"
+                  priority
+                  className="absolute w-full h-full rounded-xl"
+                  style={{ objectFit: 'cover' }}
+                />
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="w-full bg-primary-70 flex flex-col justify-center items-center">
+        <div className="mt-8 mb-8 text-center text-xl font-semibold text-primary-10">
+          상세정보
+        </div>
+        <div className="p-4 w-[327px] h-[205px] bg-slate-400">
+          지도 들어갈 자리
+        </div>
+        <div className="flex mt-2 mb-8 place-items-center text-primary-10">
+          <PiMapPin className="w-4 h-4" />
+          <p className="text-sm font-normal">{market.도로명주소}</p>
+        </div>
+      </div>
+    </>
   );
 };
 
