fd62757a5c35ed6d0b30ff207929405a133cc1da
chasyuss <cotn963@gmail.com>
2024-08-02 01:02:20 +0900

    :lipstick: style : 로그인 페이지 ui 웹에서도 보이게 수정

--- src/app/(provider)/(root)/log-in/page.tsx
+++ src/app/(provider)/(root)/log-in/page.tsx
@@ -1,21 +1,12 @@
 import React from 'react';
-import Link from 'next/link';
 import LoginHeader from './_components/LoginHeader';
 import LoginForm from './_components/LoginForm';
 
 const LoginPage = () => {
   return (
-    <div className="w-full min-h-screen bg-normal p-8 mt-2 rounded-md flex justify-center items-center">
-      <div className="w-full max-w-md">
-        <LoginHeader />
-        <LoginForm />
-        <p className="mt-6 text-center text-sm text-label-alternative">
-          아직 회원이 아니신가요?{' '}
-          <Link href="/sign-up" className="font-medium text-label-normal">
-            회원가입
-          </Link>
-        </p>
-      </div>
+    <div className="bg-normal flex flex-col items-center justify-center">
+      <LoginHeader />
+      <LoginForm />
     </div>
   );
 };
