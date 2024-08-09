39824262518ae1ff8d79c3f5b1e5432cb092529b
jognhoon <jonghoon7431@gmail.com>
2024-08-09 12:10:10 +0900

    :poop: chore : 추후 처리할 것 주석 추가

--- src/components/common/PayButton.tsx
+++ src/components/common/PayButton.tsx
@@ -20,6 +20,8 @@ export type Products = {
 interface Props {
   orderNameArr: (string | null)[];
   product: Products;
+  //TODO selectedItems PROPS로 받아서 없을 경우 버튼 스타일링 OR 비활성화하기
+  // selectedItems: string[];
 }
 
 const PayButton = ({ orderNameArr, product }: Props) => {
@@ -86,7 +88,8 @@ const PayButton = ({ orderNameArr, product }: Props) => {
             : `http://localhost:3000/check-payment?totalQuantity=${totalQuantity}`,
         noticeUrls: [
           `https://k-nostalgia.vercel.app/api/payment/webhook`,
-          'https://a8e3-118-33-158-132.ngrok-free.app/api/payment/webhook'
+          //TODO 테스트용 URL (추후 제거)
+          'https://134c-118-33-158-132.ngrok-free.app/api/payment/webhook'
         ],
 
         customer: {
