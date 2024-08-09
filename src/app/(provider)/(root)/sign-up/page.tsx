3eca339d17821cfddf2f7fd416fcaec7b9742162
chasyuss <cotn963@gmail.com>
2024-08-10 00:16:13 +0900

    :lipstick: style : 모바일 버전 hidden

--- src/app/(provider)/(root)/sign-up/page.tsx
+++ src/app/(provider)/(root)/sign-up/page.tsx
@@ -2,7 +2,7 @@ import SignUpContainer from './_components/SignUpContainer';
 
 const SignUpPage = () => {
   return (
-    <div className="flex h-screen w-screen flex-col justify-between">
+    <div className="flex h-screen w-screen flex-col justify-between xs:hidden">
       <SignUpContainer />
     </div>
   );
