4e2e65a06de0c7802a4ed7558b890c9f7a2bf05e
chasyuss <cotn963@gmail.com>
2024-08-12 11:15:33 +0900

    :lipstick: style : 웹 버전 ui

---
+++ src/app/(provider)/(root)/log-in/_components/DesktopLayout.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Image from 'next/image';
+import LoginForm from './LoginForm';
+
+const DesktopLoginLayout = () => {
+  return (
+    <div className="hidden xs:flex bg-primary-20 items-center justify-center">
+      <div className="bg-normal h-[952px] border mt-16 mb-16 rounded-[20px] overflow-hidden max-w-6xl flex w-full">
+        <div className="w-1/2 bg-normal p-8 flex flex-col justify-center items-center">
+          <Image
+            src="/image/Banner.png"
+            alt="Login illustration"
+            width={556}
+            height={888}
+            className="w-[556px] h-[888px]"
+          />
+        </div>
+
+        <div className="w-1/2 p-12 flex flex-col justify-center">
+          <Image
+            src="/image/TitleLogo.png"
+            alt="title logo"
+            priority
+            width={353}
+            height={100}
+            className="w-[353px] h-[98px] mb-12 mx-auto"
+          />
+          <LoginForm />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DesktopLoginLayout;
