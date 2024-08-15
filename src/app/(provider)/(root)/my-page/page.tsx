06c18e9154a9d3988d8354e4e4ef6f247aa93fe7
chasyuss <cotn963@gmail.com>
2024-08-15 17:46:26 +0900

    :lipstick: style : 마이페이지 웹버전 ui 진행중

--- src/app/(provider)/(root)/my-page/page.tsx
+++ src/app/(provider)/(root)/my-page/page.tsx
@@ -21,26 +21,31 @@ const Mypage = () => {
         router.push('/log-in');
       }
     }
-  }, [isLoading, user]);
+  }, [isLoading, user, router]);
 
   if (isLoading) return <Loading />;
   if (error) return <div> 로딩 에러 </div>;
 
   return (
-    <div>
-      <Profile />
-      <div className="border-4 border-[#F2F2F2]" />
-      <LikeMarket />
-      <div className="border-4 border-[#F2F2F2]" />
-      <OrderList_mypage />
-      <div className="border border-[#F2F2F2]" />
-      <Coupon_mypage />
-      <div className="border border-[#F2F2F2]" />
-      <CancelUser />
-      <div className="border border-[#F2F2F2]" />
-      <Logout />
+    <div className="md:flex md:flex-row md:max-w-[1280px]">
+      {/* Profile Section */}
+      <div className="md:w-2/3 md:ml-2 md:max-w-[503px] md:mt-10 md:md:max-h-[422px] md:p-10 md:bg-primary-70 md:rounded-xl">
+        <Profile />
+      </div>
+
+      {/* //앱버전 */}
+      <div className="md:hidden">
+        <LikeMarket />
+        <div className="border-4 border-[#F2F2F2]" />
+        <OrderList_mypage />
+        <div className="border border-[#F2F2F2]" />
+        <Coupon_mypage />
+        <div className="border border-[#F2F2F2]" />
+        <CancelUser />
+        <div className="border border-[#F2F2F2]" />
+        <Logout />
+      </div>
     </div>
   );
 };
-
 export default Mypage;
