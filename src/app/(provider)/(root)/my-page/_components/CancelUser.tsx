a35e005c4f052714dcd91aa0e3bd97f132f848b2
chasyuss <cotn963@gmail.com>
2024-08-16 17:57:00 +0900

    :adhesive_bandage: fix : 탈퇴 기능 연결

--- src/app/(provider)/(root)/my-page/_components/CancelUser.tsx
+++ src/app/(provider)/(root)/my-page/_components/CancelUser.tsx
@@ -9,7 +9,7 @@ const CancelUser = () => {
   const handleDeleteUser = async () => {
     try {
       const response = await api.auth.deleteUser();
-      console.log('탈퇴 성공');
+      alert('탈퇴 성공');
       router.push('/log-in');
     } catch (error) {
       console.error('탈퇴 에러');
@@ -19,7 +19,10 @@ const CancelUser = () => {
     <div className="p-4">
       <div className="flex items-center py-[3px]">
         <BsTrash3 className="mr-[8px] text-label-normal text-[20px]" />
-        <div onClick={handleDeleteUser}> 회원탈퇴 </div>
+        <div onClick={handleDeleteUser} className="cursor-pointer">
+          {' '}
+          회원탈퇴{' '}
+        </div>
       </div>
     </div>
   );
