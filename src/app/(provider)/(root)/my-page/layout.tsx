9e3b78bd63d1440fb302a064dd24342c2c6bbe2c
chasyuss <cotn963@gmail.com>
2024-08-18 20:08:41 +0900

    :lipstick: style : layout ui 수정

--- src/app/(provider)/(root)/my-page/layout.tsx
+++ src/app/(provider)/(root)/my-page/layout.tsx
@@ -8,15 +8,16 @@ function MyPageLayout({ children }: PropsWithChildren) {
   const { isDesktop } = useDeviceSize();
   return (
     <div
-      className={`md:min-h-screen max-w-[1280px] mx-auto md:px-4 ${
+      className={`min-h-screen max-w-[1280px] mx-auto md:px-4 ${
         isDesktop ? 'flex' : ''
       }`}
     >
       {isDesktop && (
         <aside className="w-1/3 md:mr-10">
-          <div className=" md:max-w-[503px] md:mt-10 md:md:max-h-[422px] md:p-10 md:bg-primary-70 md:rounded-xl">
+          <div className=" md:max-w-[503px] md:mt-20 md:md:max-h-[422px] md:p-10 md:bg-primary-70 md:rounded-xl">
             <Profile />
           </div>
+          <div className=" md:max-w-[503px] md:mt-20 md:md:max-h-[422px] md:p-10 md:bg-primary-70 md:rounded-xl"></div>
         </aside>
       )}
       <main className={`${isDesktop ? 'w-2/3' : 'w-full'}`}>{children}</main>
