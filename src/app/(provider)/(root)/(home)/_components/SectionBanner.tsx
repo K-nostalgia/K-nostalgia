16c9cd5152322600ee7d3a479a7d4538ea7a7d3c
t2023-m0081 <t2023-m0081@t2023-m0081ui-MacBookAir.local>
2024-08-18 23:40:36 +0900

    :lipstick: style : 메인 배너  ui

--- src/app/(provider)/(root)/(home)/_components/SectionBanner.tsx
+++ src/app/(provider)/(root)/(home)/_components/SectionBanner.tsx
@@ -48,7 +48,10 @@ export const SectionBanner = () => {
               key={index}
               className="flex justify-center items-center relative w-full"
             >
-              <Link href={index === 1 ? '/my-page/coupon-page' : '#'}>
+              <Link
+                href={index === 1 ? '/my-page/coupon-page' : '#'}
+                className="flex-1"
+              >
                 <Image
                   src={img}
                   width={isDesktop ? 1280 : 375}
@@ -67,11 +70,8 @@ export const SectionBanner = () => {
           ))}
         </CarouselContent>
         <div
-          className={`${
-            isDesktop
-              ? 'block absolute right-4 translate-x-[-10%] bottom-[5%] translate-y-[-31%] px-[10px] py-[2px] rounded-[16px] bg-[rgba(0,0,0,.24)] text-center text-label-light text-xs font-medium'
-              : 'hidden'
-          }`}
+          className={`absolute right-4 translate-x-[-10%] bottom-[1rem] translate-y-0 px-[10px] py-[2px] rounded-[16px] bg-[rgba(0,0,0,.24)] text-center text-label-light text-xs font-medium 
+        `}
         >
           {`${current} / ${count}`}
         </div>
