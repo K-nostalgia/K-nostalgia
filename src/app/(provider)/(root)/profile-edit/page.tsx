a6388c7f86f03254b7a107c9d4def7fd76b8b65f
chasyuss <cotn963@gmail.com>
2024-08-19 16:12:14 +0900

    :lipstick: style : 프로필 수정 페이지 헤더 푸터 수정

--- src/app/(provider)/(root)/profile-edit/page.tsx
+++ src/app/(provider)/(root)/profile-edit/page.tsx
@@ -14,6 +14,8 @@ import { BsChevronRight } from 'react-icons/bs';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import Swal from 'sweetalert2';
 import { toast } from '@/components/ui/use-toast';
+import useDeviceSize from '@/hooks/useDeviceSize';
+import DefaultWebLayout from '@/components/common/DefaultWebLayout';
 
 const EditProfilePage = () => {
   const { data: user, isLoading, error } = useUser();
@@ -23,6 +25,7 @@ const EditProfilePage = () => {
   const [errorMessage, setErrorMessage] = useState('');
   const router = useRouter();
   const queryClient = useQueryClient();
+  const { isDesktop } = useDeviceSize();
 
   const updateProfile = useMutation({
     mutationFn: async ({
@@ -127,96 +130,179 @@ const EditProfilePage = () => {
   if (error) return <div>Error loading user data</div>;
 
   return (
-    <DefaultAppLayout
-      showHeader={true}
-      showNavigation={true}
-      showSearch={false}
-      showCart={false}
-      showComplete={true}
-      onCompleteClick={handleEditClick}
-      headerTitle="프로필 수정"
-    >
-      <div className="p-6 rounded-lg flex flex-col items-center relative mt-20 md:mt-32">
-        <div className="hidden md:flex md:items-center md:justify-center md:absolute md:w-[240px] md:h-[152px] md:-top-9 md:z-10 ">
-          <Image
-            src="/image/Profile_Adorn.png"
-            alt="tiger overlay"
-            layout="fill"
-            objectFit="cover"
-            className="rounded-full"
-          />
-        </div>
-        <div className="relative w-[120px] md:w-[168px] md:h-[168px] h-[120px] rounded-full border border-primary-20 flex items-center justify-center">
-          {user?.avatar ? (
-            <Image
-              src={previewImage ?? '/image/profile.png'}
-              alt="profile"
-              priority
-              width={120}
-              height={120}
-              className="w-full h-full rounded-full object-cover"
-            />
-          ) : (
-            <div className="w-full h-full rounded-full flex items-center justify-center">
-              <span>프로필 사진</span>
-            </div>
-          )}
-          <label htmlFor="image-upload" className="cursor-pointer">
-            <PiArrowsLeftRight className="absolute right-0 bottom-0 text-[36px] text-label-light rounded-full bg-primary-20" />
-          </label>
-          <input
-            id="image-upload"
-            type="file"
-            accept="image/*"
-            onChange={handleImageChange}
-            className="hidden"
-          />
-        </div>
-        <div className="w-full relative mt-4 md:py-3 md:px-4 md:flex md:flex-col md:gap-2 md: items-center md:justify-center">
-          <label htmlFor="nickname" className="block text-label-normal">
-            별명
-          </label>
-          <div className="md:relative">
-            <input
-              id="nickname"
-              type="text"
-              placeholder="사용할 별명을 입력해주세요"
-              className="block w-full mt-2 p-3 px-3 border bg-white border-primary-20 focus:outline-none focus:ring-primary-20 rounded-xl text-primary-20 pr-10 md:w-[320px]"
-              value={nickname}
-              onChange={handleNicknameChange}
-            />
-            {nickname && !errorMessage ? (
-              <BsCheck2 className="absolute right-3 top-[52%] md:top-[35%] text-[24px] text-[#755428]" />
-            ) : (
-              <CgClose
-                className={`absolute right-3 ${
-                  errorMessage
-                    ? 'top-[42%] md:top-[25%]'
-                    : 'top-[52%] md:top-[35%]'
-                } text-[24px] ${
-                  errorMessage ? 'text-red-500' : 'text-label-assistive'
-                }`}
+    <>
+      {isDesktop ? (
+        <DefaultWebLayout>
+          <div className="p-6 rounded-lg flex flex-col items-center relative mt-20 md:mt-32 md:h-[80vh]">
+            <div className="hidden md:flex md:items-center md:justify-center md:absolute md:w-[240px] md:h-[152px] md:-top-9 md:z-10 ">
+              <Image
+                src="/image/Profile_Adorn.png"
+                alt="tiger overlay"
+                layout="fill"
+                objectFit="cover"
+                className="rounded-full"
               />
-            )}
-            {errorMessage && (
-              <span className=" text-red-500 text-sm ml-2">{errorMessage}</span>
-            )}
-          </div>
+            </div>
+            <div className="relative w-[120px] md:w-[168px] md:h-[168px] h-[120px] rounded-full border border-primary-20 flex items-center justify-center">
+              {user?.avatar ? (
+                <Image
+                  src={previewImage ?? '/image/profile.png'}
+                  alt="profile"
+                  priority
+                  width={120}
+                  height={120}
+                  className="w-full h-full rounded-full object-cover"
+                />
+              ) : (
+                <div className="w-full h-full rounded-full flex items-center justify-center">
+                  <span>프로필 사진</span>
+                </div>
+              )}
+              <label htmlFor="image-upload" className="cursor-pointer">
+                <PiArrowsLeftRight className="absolute right-0 bottom-0 text-[36px] text-label-light rounded-full bg-primary-20" />
+              </label>
+              <input
+                id="image-upload"
+                type="file"
+                accept="image/*"
+                onChange={handleImageChange}
+                className="hidden"
+              />
+            </div>
+            <div className="w-full relative mt-4 md:py-3 md:px-4 md:flex md:flex-col md:gap-2 md: items-center md:justify-center">
+              <label htmlFor="nickname" className="block text-label-normal">
+                별명
+              </label>
+              <div className="md:relative">
+                <input
+                  id="nickname"
+                  type="text"
+                  placeholder="사용할 별명을 입력해주세요"
+                  className="block w-full mt-2 p-3 px-3 border bg-white border-primary-20 focus:outline-none focus:ring-primary-20 rounded-xl text-primary-20 pr-10 md:w-[320px]"
+                  value={nickname}
+                  onChange={handleNicknameChange}
+                />
+                {nickname && !errorMessage ? (
+                  <BsCheck2 className="absolute right-3 top-[52%] md:top-[35%] text-[24px] text-[#755428]" />
+                ) : (
+                  <CgClose
+                    className={`absolute right-3 ${
+                      errorMessage
+                        ? 'top-[42%] md:top-[25%]'
+                        : 'top-[52%] md:top-[35%]'
+                    } text-[24px] ${
+                      errorMessage ? 'text-red-500' : 'text-label-assistive'
+                    }`}
+                  />
+                )}
+                {errorMessage && (
+                  <span className=" text-red-500 text-sm ml-2">
+                    {errorMessage}
+                  </span>
+                )}
+              </div>
 
-          <button
-            className="hidden md:flex md:border md:border-[#C8C8C8] md:hover:bg-primary-strong md:hover:text-label-light md:px-4 md:py-2 md:rounded md:bg-[#F2F2F2] md:mt-10 md:text-[#AFACA7]"
-            onClick={handleEditClick}
-          >
-            {' '}
-            수정 완료{' '}
-          </button>
-        </div>
-        {/* <div className="hidden md:flex md:flex-row md:items-center md:mt-[85px] ">
-          <p className="md:text-label-alternative md:mr-1 "> 회원탈퇴 </p>
-          <BsChevronRight className="md:text-[#838383] cursor-pointer md:w-4 md:h-4" />
-        </div> */}
-      </div>
-    </DefaultAppLayout>
+              <button
+                className="hidden md:flex md:border md:border-[#C8C8C8] md:hover:bg-primary-strong md:hover:text-label-light md:px-4 md:py-2 md:rounded md:bg-[#F2F2F2] md:mt-10 md:text-[#AFACA7]"
+                onClick={handleEditClick}
+              >
+                {' '}
+                수정 완료{' '}
+              </button>
+            </div>
+          </div>
+        </DefaultWebLayout>
+      ) : (
+        <DefaultAppLayout
+          showHeader={true}
+          showNavigation={true}
+          showSearch={false}
+          showCart={false}
+          showComplete={true}
+          onCompleteClick={handleEditClick}
+          headerTitle="프로필 수정"
+        >
+          <div className="p-6 rounded-lg flex flex-col items-center relative mt-20 md:mt-32">
+            <div className="hidden md:flex md:items-center md:justify-center md:absolute md:w-[240px] md:h-[152px] md:-top-9 md:z-10 ">
+              <Image
+                src="/image/Profile_Adorn.png"
+                alt="tiger overlay"
+                layout="fill"
+                objectFit="cover"
+                className="rounded-full"
+              />
+            </div>
+            <div className="relative w-[120px] md:w-[168px] md:h-[168px] h-[120px] rounded-full border border-primary-20 flex items-center justify-center">
+              {user?.avatar ? (
+                <Image
+                  src={previewImage ?? '/image/profile.png'}
+                  alt="profile"
+                  priority
+                  width={120}
+                  height={120}
+                  className="w-full h-full rounded-full object-cover"
+                />
+              ) : (
+                <div className="w-full h-full rounded-full flex items-center justify-center">
+                  <span>프로필 사진</span>
+                </div>
+              )}
+              <label htmlFor="image-upload" className="cursor-pointer">
+                <PiArrowsLeftRight className="absolute right-0 bottom-0 text-[36px] text-label-light rounded-full bg-primary-20" />
+              </label>
+              <input
+                id="image-upload"
+                type="file"
+                accept="image/*"
+                onChange={handleImageChange}
+                className="hidden"
+              />
+            </div>
+            <div className="w-full relative mt-4 md:py-3 md:px-4 md:flex md:flex-col md:gap-2 md: items-center md:justify-center">
+              <label htmlFor="nickname" className="block text-label-normal">
+                별명
+              </label>
+              <div className="md:relative">
+                <input
+                  id="nickname"
+                  type="text"
+                  placeholder="사용할 별명을 입력해주세요"
+                  className="block w-full mt-2 p-3 px-3 border bg-white border-primary-20 focus:outline-none focus:ring-primary-20 rounded-xl text-primary-20 pr-10 md:w-[320px]"
+                  value={nickname}
+                  onChange={handleNicknameChange}
+                />
+                {nickname && !errorMessage ? (
+                  <BsCheck2 className="absolute right-3 top-[52%] md:top-[35%] text-[24px] text-[#755428]" />
+                ) : (
+                  <CgClose
+                    className={`absolute right-3 ${
+                      errorMessage
+                        ? 'top-[42%] md:top-[25%]'
+                        : 'top-[52%] md:top-[35%]'
+                    } text-[24px] ${
+                      errorMessage ? 'text-red-500' : 'text-label-assistive'
+                    }`}
+                  />
+                )}
+                {errorMessage && (
+                  <span className=" text-red-500 text-sm ml-2">
+                    {errorMessage}
+                  </span>
+                )}
+              </div>
+
+              <button
+                className="hidden md:flex md:border md:border-[#C8C8C8] md:hover:bg-primary-strong md:hover:text-label-light md:px-4 md:py-2 md:rounded md:bg-[#F2F2F2] md:mt-10 md:text-[#AFACA7]"
+                onClick={handleEditClick}
+              >
+                {' '}
+                수정 완료{' '}
+              </button>
+            </div>
+          </div>
+        </DefaultAppLayout>
+      )}
+    </>
   );
 };
 export default EditProfilePage;
