4bd109e412dfe571c839c547dbd75bb7197fd940
chasyuss <cotn963@gmail.com>
2024-08-19 17:56:43 +0900

    :lipstick: style : 마이페이지 왼쪽 layout에 배너 추가

--- src/app/(provider)/(root)/my-page/layout.tsx
+++ src/app/(provider)/(root)/my-page/layout.tsx
@@ -3,6 +3,7 @@
 import useDeviceSize from '@/hooks/useDeviceSize';
 import { PropsWithChildren } from 'react';
 import Profile from './_components/Profile';
+import Image from 'next/image';
 // import RecentMarket from './_components/RecentMarket';
 
 function MyPageLayout({ children }: PropsWithChildren) {
@@ -18,9 +19,16 @@ function MyPageLayout({ children }: PropsWithChildren) {
           <div className="w-full max-w-[503px] md:mt-20 md:max-h-[422px] md:p-10 md:bg-primary-70 md:rounded-xl">
             <Profile />
           </div>
-          <div className="w-full max-w-[503px] md:mt-20 md:max-h-[422px] md:p-10 md:bg-secondary-70 md:rounded-xl">
-            {/* <RecentMarket /> */}
-          </div>
+          {/* <div className="w-full max-w-[503px] md:mt-20 md:max-h-[422px] md:p-10 md:bg-secondary-70 md:rounded-xl"> */}
+          {/* <RecentMarket /> */}
+          <Image
+            src="/image/ProfileBanner.png"
+            alt="divider"
+            width={503}
+            height={900}
+            className="w-[503px] h-[900px] mt-6"
+          />
+          {/* </div> */}
         </aside>
       )}
       <main className={`${isDesktop ? 'w-2/3 ' : 'w-full'}`}>{children}</main>
