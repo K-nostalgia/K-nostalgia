c1dfabb1e2cebb8a1b3a72181c985724c7ae5d38
Sera Choi <c.sera2511@gmail.com>
2024-08-19 16:17:17 +0900

    chore: merge

--- src/components/chat/_component/ChatList.tsx
+++ src/components/chat/_component/ChatList.tsx
@@ -87,6 +87,13 @@ const ChatList = ({
           </div>
         ))}
       </div>
+      <div
+        onClick={() => setShowWarning(true)}
+        className="underline text-nowrap text-center text-sm text-label-alternative font-medium leading-[19.6px] cursor-pointer md:pb-12"
+      >
+        채팅방 이용시 주의사항
+      </div>
+      {showWarning && <WarningChat setShowWarning={setShowWarning} />}
     </DialogContent>
   );
 };
