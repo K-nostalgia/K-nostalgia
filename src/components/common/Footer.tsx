560945a3eef86453bfe6d21117a87d16ecccda67
Sera Choi <c.sera2511@gmail.com>
2024-08-28 13:37:41 +0900

    :sparkles: feat: udpate footer on _blank github and reserved name

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -8,7 +8,11 @@ const Footer = () => {
     <div className="h-[108px] bg-normal border-t border-[#C8C8C8] z-50">
       <div className="max-w-screen-xl mx-auto flex justify-between items-center mt-[22px] md:px-3 xl:px-0">
         <div className="cursor-pointer">
-          <Link href="https://github.com/Chasyuss/K-nostalgia" className="flex">
+          <Link
+            href="https://github.com/Chasyuss/K-nostalgia"
+            target="_blank"
+            className="flex"
+          >
             <Image
               src="/image/GitHub.png"
               alt="깃허브"
