b05bdd35374395844e1ef2d3ef29f006b716e1cc
jonghoon <jonghoon7431@gmail.com>
2024-09-13 17:19:53 +0900

    refactor: layout 서버 컴포넌트로 변경, (useDesktop제거/css로 미디어 쿼리 적용) - 초기 로드 속도 100ms감소

--- src/app/(root)/payment/page.tsx
+++ src/app/(root)/payment/page.tsx
@@ -1,10 +1,4 @@
 import PayHistoryList from '@/app/(root)/payment/_components/PayHistoryList';
-import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: '향그리움-주문내역',
-  description: '구매하신 특산품 주문 내역을 확인하실 수 있는 페이지입니다.'
-};
 
 const PaymentList = () => {
   return (
