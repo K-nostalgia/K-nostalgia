162868e73795320e37239271e863f757f7e68623
chasyuss <cotn963@gmail.com>
2024-09-14 22:13:26 +0900

    :lipstick: style : 고객센터 앱버전 ui

---
+++ src/app/(root)/customer-service/_components/CustomerCard.tsx
@@ -0,0 +1,45 @@
+import Announcement from '@/components/icons/Announcement';
+import BulkOrder from '@/components/icons/BulkOrder';
+import FAQ from '@/components/icons/FAQ';
+import SellerRegister from '@/components/icons/SellerRegister';
+import React from 'react';
+
+const CustomerCard = () => {
+  return (
+    <div className="grid grid-cols-2 gap-3 mt-6">
+      {/* 공지사항 */}
+      <div className="cursor-pointer flex flex-col items-center justify-center border border-primary-20 bg-white rounded-[6px] w-[166px] h-[140px]">
+        <Announcement />
+        <span className="mt-[10px] text-primary-10 font-medium text-base">
+          공지사항
+        </span>
+      </div>
+
+      {/* 자주 묻는 질문 */}
+      <div className="cursor-pointer flex flex-col items-center justify-center border border-primary-20 bg-white rounded-[6px] w-[166px] h-[140px]">
+        <FAQ />
+        <span className="mt-[10px] text-primary-10 font-medium text-base">
+          자주 묻는 질문
+        </span>
+      </div>
+
+      {/* 대량 주문 문의 */}
+      <div className="cursor-pointer flex flex-col items-center justify-center border border-primary-20 bg-white rounded-[6px] w-[166px] h-[140px]">
+        <BulkOrder />
+        <span className="mt-[10px] text-primary-10 font-medium text-base">
+          대량 주문 문의
+        </span>
+      </div>
+
+      {/* 판매자 등록 문의 */}
+      <div className="cursor-pointer flex flex-col items-center justify-center border border-primary-20 bg-white rounded-[6px] w-[166px] h-[140px]">
+        <SellerRegister />
+        <span className="mt-[10px] text-primary-10 font-medium text-base">
+          판매자 등록 문의
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default CustomerCard;
---
+++ src/app/(root)/customer-service/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import CustomerHeader from './_components/CustomerHeader';
+import CustomerContent from './_components/CustomerContent';
+
+const CustomerPage = () => {
+  return (
+    <div>
+      {' '}
+      <CustomerHeader />
+      <CustomerContent />
+    </div>
+  );
+};
+
+export default CustomerPage;
