7470980819c25ae3757bf3026adb2d5d968b9ac1
jonghoon <jonghoon7431@gmail.com>
2024-09-30 17:32:32 +0900

    chore: todo 주석 추가

--- src/app/(root)/payment/_components/PayHistoryItem.tsx
+++ src/app/(root)/payment/_components/PayHistoryItem.tsx
@@ -54,17 +54,19 @@ const PayHistoryItem = ({ orderList, date }: Props) => {
   //취소 및 db 업데이트
   const cancelPaymentMutation = usePaymentCancellation();
 
+  //TODO 주문 취소시에 해당 건 리뷰 삭제
+
   const cancelPayment = async (order: BaseOrderInPayHistory) => {
     Swal.fire({
       title: '구매 상품을 환불하시겠어요?',
       html: `
-        <div id="swal2-html-container" class="swal2-html-container" style=" padding:0 !important; margin:-1rem; font-size:16px;">환불 후에는 리뷰 작성 및 환불 취소가 불가해요.</div>
+        <div id="swal2-html-container" class="swal2-html-container" style=" padding:0 !important; margin:-1rem; font-size:16px;">환불 후에는 리뷰 작성 및 환불 취소가 불가해요</div>
       `,
       showCancelButton: true,
       cancelButtonColor: '#9C6D2E',
       confirmButtonColor: '#f2f2f2',
-      cancelButtonText: '환불 취소',
-      confirmButtonText: '환불하기',
+      cancelButtonText: '취소',
+      confirmButtonText: '환불',
       customClass: {
         title: 'text-xl mt-10 md:mb-[8px]',
         popup: 'rounded-[16px]',
