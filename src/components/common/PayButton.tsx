f26e70852fc6d01690da9ad234cc76bcbc3fa776
jognhoon <jonghoon7431@gmail.com>
2024-10-08 15:07:16 +0900

    fix: 오타 수정

--- src/components/common/PayButton.tsx
+++ src/components/common/PayButton.tsx
@@ -116,9 +116,7 @@ const PayButton = ({ orderNameArr, product, text }: Props) => {
           process.env.NODE_ENV === 'production'
             ? `https://https://k-nostalgia-one.vercel.app/check-payment?totalQuantity=${totalQuantity}`
             : `http://localhost:3000/check-payment?totalQuantity=${totalQuantity}`,
-        noticeUrls: [
-          `https://https://k-nostalgia-one.vercel.app/api/payment/webhook`
-        ],
+        noticeUrls: [`https://k-nostalgia-one.vercel.app/api/payment/webhook`],
 
         customer: {
           customerId: id,
@@ -136,9 +134,9 @@ const PayButton = ({ orderNameArr, product, text }: Props) => {
           }
         }
       });
+
       const paymentId = response?.paymentId;
-      console.log(response);
-      //TODO 에러 코드에 따라 토스트 다르게 띄우기
+
       if (response?.code != null) {
         toast({
           variant: 'destructive',
